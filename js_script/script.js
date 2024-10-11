2edf2d62345a9140309696f72173a90ae5787b69
dockerwin11 <86618518+dockerwin11@users.noreply.github.com>
2024-10-12 00:35:58 +0800

    Add files via upload

--- js_script/script.js
+++ js_script/script.js
@@ -14,12 +14,22 @@ $(document).ready(function() {
 
         // Get the input values
         let dj_number = $('#dj').val();
-        //const age = $('#age').val();
 
         dj_number = dj_number * 5;
 
         // Display the values
         console.log('DJ number:', dj_number);
-        //console.log('Age:', age);
+    });
+
+    $('#toy-form').on('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // Get the input values
+        let toy_number = $('#toy').val();
+
+        toy_number = toy_number * 3;
+
+        // Display the values
+        console.log('toy number:', toy_number);
     });
 });
