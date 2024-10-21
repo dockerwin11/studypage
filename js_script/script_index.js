991dd84a1cffcd7d04240d4c59e9f7e47b99efd1
dockerwin11 <86618518+dockerwin11@users.noreply.github.com>
2024-10-21 23:56:23 +0800

    Add files via upload

---
+++ js_script/script_index.js
@@ -0,0 +1,49 @@
+function main() {
+    window.open(`en/main.html` , `_self`);
+  }
+  function main_en() {
+    window.open(`main.html` , `_self`);
+  }
+  function main_tc() {
+    window.open(`main.html` , `_self`);
+  }
+  function main_sc() {
+    window.open(`main.html` , `_self`);
+  }
+function cover1() {
+  window.open(`en/38-Surf.html` , `_blank`);
+}
+function cover2() {
+  window.open(`en/Regal-LS4.html` , `_blank`);
+}
+function cover3() {
+  window.open(`en/Regal-42-Fly.html` , `_blank`);
+}
+function cover4() {
+  window.open(`en/Regal-38-Grande.html` , `_blank`);
+}
+function cover5() {
+  window.open(`en/Beneteau-Oceanis-60.html` , `_blank`);
+}
+function cover6() {
+  window.open(`en/Bali-5-8.html` , `_blank`);
+}
+
+function en_cover1() {
+  window.open(`38-Surf.html` , `_blank`);
+}
+function en_cover2() {
+  window.open(`Regal-LS4.html` , `_blank`);
+}
+function en_cover3() {
+  window.open(`Regal-42-Fly.html` , `_blank`);
+}
+function en_cover4() {
+  window.open(`Regal-38-Grande.html` , `_blank`);
+}
+function en_cover5() {
+  window.open(`Beneteau-Oceanis-60.html` , `_blank`);
+}
+function en_cover6() {
+  window.open(`Bali-5-8.html` , `_blank`);
+}
