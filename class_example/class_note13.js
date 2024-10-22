7c1fab0e73ceb094007c475cfe4b1e991e1269fc
dockerwin11 <86618518+dockerwin11@users.noreply.github.com>
2024-10-22 17:38:14 +0800

    Add files via upload

---
+++ class_example/class_note13.js
@@ -0,0 +1,52 @@
+x = a => b => a + b;
+
+x = function (a) { return function (b) { return a + b;}};
+
+console.log(x(1)(2));
+
+
+
+b = function (a+2) { console.log(c); };
+console.log(b);
+
+x = () => {
+    let x = 8;
+    let y = 9;
+    console.log("I am inside arrow function");
+    return [x, y];
+}
+console.log(x());
+[a,b]=x();
+
+console.log(x()[1]);
+
+console.log(a,b);
+
+let companys;
+const companies = [
+    {name: "company one", category: "retails", start: 1992, end: 2024},
+    {name: "company two", category: "finance", start: 1971, end: 2020},
+    {name: "company three", category: "wholeSales", start: 1988, end: 1999},
+    {name: "company four", category: "wholeSales", start: 1996, end: 2010},
+    {name: "company five", category: "retails", start: 1960, end: 2004},
+    {name: "company six", category: "finance", start: 1992, end: 2011},
+    {name: "company seven", category: "finance", start: 1986, end: 2009},
+    {name: "company eight", category: "retails", start: 1977, end: 2012},
+    {name: "company nine", category: "retails", start: 1992, end: 2006},
+    {name: "company ten", category: "wholeSales", start: 1992, end: 1997}
+];
+
+const ages = [33,12,45,77,60,23,7,80.55,67,29,16];
+
+for (let i=0; i < companies.length; i++) {
+    console.log(companies[i]);
+}
+
+companies.forEach(companys = (company) => { console.log(company.start)});
+console.log(companys);
+
+companies.forEach(company=> { 
+    console.log(company.start);
+    company.start++;
+    console.log(company.start);
+});
