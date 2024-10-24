c97edd0ad3e01d25b452c798cc945e1461439de8
billy <billywongkk@yahoo.com.hk>
2024-10-24 16:05:16 +0800

    second class commit

--- class_example/class_note16.js
+++ class_example/class_note16.js
@@ -12,39 +12,52 @@ z[0]();
 a.fun();
 console.log(y);
 
-a = [5,8,6,10];
+aa = [2,33];
 max_value = (a,b) => (a > b ? a:b);
 function max_sequence(a) {
-    let result = a[0];
-    for (let e of a) {
+    let result = aa[0];
+    for (let e of aa) {
         result = max_value(result,e);
     }
     return result;
 }
-console.log(max_sequence(a));
+console.log(max_sequence(aa));
 
 // case 2
 min_value = (a,b) => (a < b ? a:b);
 function min_sequence(a) {
-    let result = a[0];
-    for (let e of a) {
+    let result = aa[0];
+    for (let e of aa) {
         result = min_value(result,e);
     }
     return result;
 }
-console.log(min_sequence(a));
+console.log(min_sequence(aa));
 
 // case 3
-function reduce(fn, a) {
-    let result = a[0];
-    for (let e of a) {
-        result = fn(result, e);
+function reduce(fn, aa) {
+    if (aa.length === 0) {
+        console.log("can't compute empty array");
     }
+    if (aa.length === 1) {
+         return aa[0];
+    }
+    if (aa.length > 1) {
+        let result = aa[0];
+        for (let x = 1; x < aa.length; x++) {
+            result = fn(result, aa[x]);
+        }
+    
     return result;
+    }
+
 }
-console.log(reduce(max_value, a))
-console.log(reduce(min_value, a))
+
+console.log(reduce(max_value, aa));
+console.log(reduce(min_value, aa));
+console.log(reduce((a,b)=>a+b,aa));
 
 
 result = a.reduce((a,b) => a > b ? a: b);
 console.log(result);
+
